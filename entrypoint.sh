b461ca80ab1a048b2a002f0c7ef2b16ec3065d8d
Rallenson <Rallenson90@gmail.com>
2025-06-03 19:48:53 -0300

    fix: garantir a criação do banco de dados SQLite se não existir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Garante que o banco existe (caso o COPY não tenha trazido)
+if [ ! -f /var/www/database/database.sqlite ]; then
+    mkdir -p /var/www/database
+    touch /var/www/database/database.sqlite
+    chmod 777 /var/www/database/database.sqlite
+fi
+
 # Garante que o storage e cache tenham as permissões corretas
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
