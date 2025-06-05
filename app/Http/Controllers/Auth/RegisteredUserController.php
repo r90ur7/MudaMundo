0020b24506931f90fe139edf2495a4ee15986470
Rallenson <Rallenson90@gmail.com>
2025-06-05 00:38:26 -0300

    fix: rm test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -54,8 +54,6 @@ class RegisteredUserController extends Controller
             'lgpd_consent_at' => now(),
         ]);
 
-        dd($user, 'criei');
-
         event(new Registered($user));
 
         Auth::login($user);
