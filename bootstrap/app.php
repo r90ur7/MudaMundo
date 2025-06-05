807ea091bb80072bbb6885535ece3ac5d5838dac
Rallenson <Rallenson90@gmail.com>
2025-06-04 23:27:31 -0300

    fix: force htps in prod

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-        $middleware->validateCsrfTokens(except: ['/*']);
+        $middleware->append(\App\Http\Middleware\ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
