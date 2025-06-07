1a6414abaa90eb554c0483865ac0a874fe9f1095
Rallenson <Rallenson90@gmail.com>
2025-06-06 23:43:35 -0300

    fix: tentativa de resolver o problema da queue

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,5 +26,4 @@ echo "Migrations finalizadas."
 php artisan optimize:clear
 
 # Inicia o servidor PHP embutido na porta 10000
-php -S 0.0.0.0:10000 -t public
-php artisan queue:work
+php artisan queue:work & php -S 0.0.0.0:10000 -t public
