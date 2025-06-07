9e9031dcf9d6449f6de721eaa47f3276c91c14cd
Rallenson <Rallenson90@gmail.com>
2025-06-06 22:16:26 -0300

    fix: route test all users api

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 // API de chats do usuário
 Route::middleware('auth:sanctum')->get('/chats', [\App\Http\Controllers\SolicitacaoMensagemController::class, 'userChats']);
+
+
+Route::get('all-users', function () {
+    return response()->json(\App\Models\User::all());
+});
